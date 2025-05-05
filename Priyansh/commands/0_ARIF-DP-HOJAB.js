b1e0de679681ffc205b689e37ff6371e98084017
MD SIRAJ <siiraaj16@gmail.com>
2025-05-05 21:15:50 +0300

    Update 0_ARIF-DP-HOJAB.js

--- Priyansh/commands/0_ARIF-DP-HOJAB.js
+++ Priyansh/commands/0_ARIF-DP-HOJAB.js
@@ -23,6 +23,8 @@ const fs = global.nodemodule["fs-extra"];
     var link = [
 "https://i.imgur.com/tPvqrVH.jpg","https://i.imgur.com/1M123yS.jpg","https://i.imgur.com/okpdmFt.jpg","https://i.imgur.com/VY08K5y.jpg","https://i.imgur.com/An91n1s.jpg","https://i.imgur.com/ENb9RAp.jpg","https://i.imgur.com/aAUBos2.jpg","https://i.imgur.com/GZRyD6t.jpg","https://i.imgur.com/aVOY30b.jpg","https://i.imgur.com/lh0EtJx.jpg","https://i.imgur.com/qajIAts.jpg","https://i.imgur.com/IiOJVjq.jpg","https://i.imgur.com/W8M7aML.jpg","https://i.imgur.com/EPgAZYe.jpg","https://i.imgur.com/bSVVkv4.jpg","https://i.imgur.com/pKqztui.jpg","https://i.imgur.com/p2Jhu2W.jpg","https://i.imgur.com/a0VKSjy.jpg","https://i.imgur.com/8LX1VuZ.jpg","https://i.imgur.com/7wcCIjZ.jpg","https://i.imgur.com/dRvwevs.jpg","https://i.imgur.com/zR3O2jc.jpg","https://i.imgur.com/AcUos3J.jpg","https://i.imgur.com/4VwjQvh.jpg","https://i.imgur.com/6h7BRr9.jpg",
      ];
-     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     𝐒𝐚𝐦𝐞𝐞𝐫 𝐊𝐡𝐚𝐧                    ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\n♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+     var callback = () => api.sendMessage({body:`┏━━━━━┓\n    ╔╬⓼★⓼╃───────➣™
+🄼🅁 🄱🄾🅃 🄾🅆🄽🄴🅁 🅂🄰🄼🄴🄴🅁𝄠━─━Ⓔ⧐
+╰✾✾®️╀✿✿╀─━ↈⓇ⧐                   ✧══•❁😛❁•══✧\n┗━━━━━┛\n\n♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    }
