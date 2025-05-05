0d3d5559a81e5d2a4f995b03a947c835aa5abdf3
MD SIRAJ <siiraaj16@gmail.com>
2025-05-05 21:22:36 +0300

    Update 0_ARIF-PAIRV--.js

--- Priyansh/commands/0_ARIF-PAIRV--.js
+++ Priyansh/commands/0_ARIF-PAIRV--.js
@@ -60,7 +60,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
   var background = [
-  "https://i.postimg.cc/zfdB9CBd/v1jFrJl.jpg"
+  "https://i.imghippo.com/files/QR9177WE.jpeg"
   ];
   var rd = background[Math.floor(Math.random() * background.length)];
 
