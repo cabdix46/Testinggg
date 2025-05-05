b9242d6a723bf883889ad82e83620952be68ddce
MD SIRAJ <siiraaj16@gmail.com>
2025-05-05 21:34:49 +0300

    Update sub-msg.js

--- Priyansh/commands/sub-msg.js
+++ Priyansh/commands/sub-msg.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Priyansh rajput")==0 || event.body.indexOf("Sub")==0 || event.body.indexOf("Subscribe")==0 || event.body.indexOf("Priyansh")==0) {
+	if (event.body.indexOf("Sameer khan")==0 || event.body.indexOf("Sub")==0 || event.body.indexOf("Subscribe")==0 || event.body.indexOf("Sameer")==0) {
 		var msg = {
-				body: "👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇",
+				body: "👋For Any Kind Of Help Contact On Telegram  Username 👉 @Sameerkhan😇",
 				attachment: fs.createReadStream(__dirname + `/noprefix/sub.mp3`)
 			}
 			api.sendMessage(msg, threadID, messageID);
